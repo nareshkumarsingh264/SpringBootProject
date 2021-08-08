95f1bb9a338731610bf461078eed8ddb31958720
DELL <nareshkumarsingh264@gmail.com>
2021-08-07 22:00:10 -0700

    first commit

---
+++ SpringBootModelInterface/src/main/java/com/naresh/SpringBootModelInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.naresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModelInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModelInterfaceApplication.class, args);
+	}
+
+}
