10008ccae32ecc66ed54fc1b0b54eeca6e37fdc2
DELL <nareshkumarsingh264@gmail.com>
2021-08-08 21:08:41 -0700

    first commit

---
+++ SpringBootRestFulWebService-1/src/main/java/com/naresh/SpringBootRestFulWebService1Application.java
@@ -0,0 +1,13 @@
+package com.naresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestFulWebService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestFulWebService1Application.class, args);
+	}
+
+}
---
+++ SpringBootRestFulWebService-1/src/main/java/com/naresh/util/ErrorType.java
@@ -0,0 +1,14 @@
+package com.naresh.util;
+
+public class ErrorType {
+
+    private String errorMessage;
+
+    public ErrorType(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
