d4bf596291f5477c54b70cb7e8da3313ffd35fa0
DELL <nareshkumarsingh264@gmail.com>
2021-08-09 20:22:58 -0700

    first commit

---
+++ SpringBootFormHandling/src/test/java/com/naresh/SpringBootFormHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
